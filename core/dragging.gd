f3a8e27e157c9db22ae51ed40abb81a7304abb05
erengee <kutaxoff@icloud.com>
2025-04-12 20:25:58 +0300

    Add dragging zones and check if objects are fully inside them

--- core/dragging.gd
+++ core/dragging.gd
@@ -12,13 +12,15 @@ var selected_object: Node2D = null
 
 var selected_object_z_index := -1
 
+var dragging_zones: Array[Area2D] = []
+
 signal object_selected(object: Node2D)
 
 func drag_object_start(object: Node2D):
 	for node in get_tree().get_nodes_in_group("draggable"):
 		if node.z_index > object.z_index:
 			node.z_index -= 1
-	
+
 	is_dragging = true
 	object.z_index = max_z_index
 
@@ -28,11 +30,11 @@ func drag_object_end(_object: Node2D):
 func select_object(object: Node2D):
 	if is_dragging && selected_object == object:
 		return
-	if (selected_object == null 
+	if (selected_object == null
 		|| is_on_top(object, selected_object)):
 		_set_selected_object(object)
 	hovered_objects.append(object)
-	
+
 func deselect_object(object: Node2D):
 	if is_dragging && selected_object == object:
 		return
@@ -46,13 +48,15 @@ func deselect_object(object: Node2D):
 				if is_on_top(obj, top_object):
 					top_object = obj
 			_set_selected_object(top_object)
-		
+
+func fully_outside_of_dragging_zone():
+	return dragging_zones.size() == 0
+
 func _set_selected_object(object: Node2D):
 	selected_object = object
 	selected_object_z_index = object.z_index if object != null else -1
 	object_selected.emit(object)
-		
+
 func is_on_top(a: Node2D, b: Node2D):
 	return (a.z_index > b.z_index
 			|| (a.z_index == b.z_index && a.is_greater_than(b)))
-	
--- scenes/draggable_object/draggable_object.gd
+++ scenes/draggable_object/draggable_object.gd
@@ -6,14 +6,20 @@ var draggable := false
 var zone_refs: Array[Node2D]
 var drag_position_delta: Vector2
 var initial_position: Vector2
+var is_inside_dragging_zone := true
 
 var tween: Tween
 
 @export var target_zone_id = "squareHole"
 
+@onready var raycastDown := $RayCast2DDown
+@onready var raycastUp := $RayCast2DUp
+@onready var raycastLeft := $RayCast2DLeft
+@onready var raycastRight := $RayCast2DRight
+
 func _ready() -> void:
 	Dragging.object_selected.connect(select)
-	
+
 func select(object: Node2D):
 	if object == self:
 		draggable = true
@@ -21,7 +27,15 @@ func select(object: Node2D):
 	else:
 		draggable = false
 		scale = Vector2(1, 1)
-		
+
+func _physics_process(delta: float) -> void:
+	var collisions = [
+		raycastDown.is_colliding(),
+		raycastUp.is_colliding(),
+		raycastRight.is_colliding(),
+		raycastLeft.is_colliding()
+	]
+	is_inside_dragging_zone = not true in collisions
 
 func _process(delta: float) -> void:
 	if draggable:
@@ -35,30 +49,42 @@ func _process(delta: float) -> void:
 			Dragging.drag_object_end(self)
 			if is_inside_drop_zone():
 				var target_zone = find_target_zone()
-				if tween:
-					tween.kill()
-				tween = get_tree().create_tween()
 				if target_zone:
-					tween.tween_property(
-						self, 
-						"position", 
-						target_zone.position, 
-						snap_duration
-					).set_ease(Tween.EASE_OUT)
+					snap_to_zone(target_zone)
 				else:
-					tween.tween_property(
-						self, 
-						"global_position", 
-						initial_position, 
-						snap_duration
-					).set_ease(Tween.EASE_OUT)
-					
+					reset_position()
+			elif !is_inside_dragging_zone || Dragging.fully_outside_of_dragging_zone():
+				reset_position()
+
+
+func reset_position():
+	if tween:
+		tween.kill()
+	tween = get_tree().create_tween()
+	tween.tween_property(
+		self,
+		"global_position",
+		initial_position,
+		snap_duration
+	).set_ease(Tween.EASE_OUT)
+
+func snap_to_zone(zone: Node2D):
+	if tween:
+		tween.kill()
+	tween = get_tree().create_tween()
+	tween.tween_property(
+		self,
+		"position",
+		zone.position,
+		snap_duration
+	).set_ease(Tween.EASE_OUT)
+
 func is_inside_drop_zone() -> bool:
 	return zone_refs.size()
-	
+
 func is_target_zone(zone: Node2D) -> bool:
 	return zone.has_method("get_zone_id") && zone.get_zone_id() == target_zone_id
-	
+
 func find_target_zone() -> Node2D:
 	var i = zone_refs.find_custom(is_target_zone.bind())
 	if i == -1:
