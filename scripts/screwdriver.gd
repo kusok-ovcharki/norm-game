83703f0a1f517eb4967b60551cc6de1d3230bbd5
kusok-ovcharki <mzhiglin@yandex.ru>
2025-04-13 14:48:20 +0300

    Add screwdriver and screw

--- scripts/screwdriver.gd
+++ scripts/screwdriver.gd
@@ -7,20 +7,25 @@ var zone_refs: Array[Node2D]
 var drag_position_offset: Vector2
 var initial_position: Vector2
 var is_inside_dragging_zone := true
-
+var initial_rotation: float
+var current_frame := 0  # Текущий кадр анимации
 var tween: Tween
 
 @export var target_zone_id = "defaultScrew"
+@export var target_rotation_degrees: float = 180.0  # Угол, на который повернётся объект
+@export var rotation_duration: float = 0.5        # Длительность анимации в секундах
 
 @onready var raycastDown := $RayCast2DDown
 @onready var raycastUp := $RayCast2DUp
 @onready var raycastLeft := $RayCast2DLeft
 @onready var raycastRight := $RayCast2DRight
-
+@onready var animated_sprite = $AnimatedSprite2D
 @onready var area := $Area2D
 
 func _ready() -> void:
 	Dragging.object_selected.connect(select)
+	initial_rotation = rotation_degrees
+	animated_sprite.play("idle")
 
 func select(object: Node2D):
 	if object == self:
@@ -39,6 +44,7 @@ func _physics_process(delta: float) -> void:
 	]
 	is_inside_dragging_zone = not true in collisions
 
+
 func _process(delta: float) -> void:
 	if draggable:
 		if Input.is_action_just_pressed("click"):
@@ -50,6 +56,25 @@ func _process(delta: float) -> void:
 		elif Input.is_action_just_released("click"):
 			handle_reposition()
 
+############# Откручивание ##########
+
+func _input(event):
+	if event is InputEventMouseButton:
+		# Обработка прокрутки вверх
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP and event.pressed:
+			advance_animation()
+
+func advance_animation():
+	# Получаем количество кадров в текущей анимации
+	var frame_count = animated_sprite.sprite_frames.get_frame_count("unscrewing")
+	# Переключаем кадр
+	current_frame = clampi(current_frame + 1, 0, frame_count - 1)
+	animated_sprite.frame = current_frame
+	# Если достигнут последний кадр
+	if current_frame == frame_count - 1:
+		current_frame = -1
+
+#######################################
 
 func handle_reposition():
 	Dragging.drag_object_end(self)
@@ -107,15 +132,22 @@ func _on_area_2d_handle_mouse_exited() -> void:
 	if not Dragging.is_dragging || Dragging.selected_object != self:
 		Dragging.deselect_object(self)
 
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("droppable"):
 		zone_refs.append(body)
+		rotate_to_target(target_rotation_degrees)
+		animated_sprite.play("unscrewing")
+		animated_sprite.pause()
+		animated_sprite.frame = 0
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.is_in_group("droppable"):
 		zone_refs.erase(body)
+		rotate_to_target(initial_rotation)
+		if rotation_degrees == target_rotation_degrees:
+			animated_sprite.play("idle")
+			
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
@@ -139,3 +171,14 @@ func _notification(what):
 	if what == NOTIFICATION_WM_MOUSE_EXIT && Dragging.selected_object == self:
 		Dragging.deselect_object(self, true)
 		handle_reposition()
+
+func rotate_to_target(rotation_degrees: float):
+	if tween:
+		tween.kill()  # Останавливаем предыдущий твин, если был
+	tween = create_tween()
+	tween.tween_property(
+		self, 
+		"rotation_degrees", 
+		rotation_degrees, 
+		rotation_duration
+	).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
